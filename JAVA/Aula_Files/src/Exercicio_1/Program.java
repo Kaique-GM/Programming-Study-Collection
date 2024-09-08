7241aa7303e8990c98fdc815fabbd05148ff7938
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-08 14:40:02 -0300

    (Java)Exercício de leitura e escrita de arquivos(Finished)

--- JAVA/Aula_Files/src/Exercicio_1/Program.java
+++ JAVA/Aula_Files/src/Exercicio_1/Program.java
@@ -14,38 +14,63 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		List<Produto> produtos = new ArrayList<>();
-
+		///////////////////////////////////////////////////////////////////
+		//Leitura
 		System.out.println("Digite o caminho para o arquivo: ");
 		String path = sc.nextLine();
-		
-		//Lê o arquivo e adiciona produtos na lista "produtos"
+
 		System.out.println("Leitura do Arquivo:");
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 
 			while (line != null) {
 				System.out.println(line);
-				
+
 				String[] itens = line.split(", ");
 				String name = itens[0];
 				double price = Double.parseDouble(itens[1]);
 				int quantity = Integer.parseInt(itens[2]);
-				
-				produtos.add(new Produto(name,price,quantity));
-				
+
+				produtos.add(new Produto(name, price, quantity));
+
 				line = br.readLine();
 			}
 
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
+		System.out.println();
+		////////////////////////////////////////////////////////////////////////
+		//Escrita
 		System.out.println("//////////////////////////////////////////////////");
-		System.out.println("Digite o caminho para o arquivo \"Summary.csv\": ");
-		System.out.println("Escrita do Arquivo \"Summary.csv\": ");
-		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter() {
-			
-			
+		System.out.println();
+		System.out.println("Digite o caminho para escrever no arquivo \"Summary.csv\": ");
+		String path2 = sc.nextLine();
+		System.out.println("Escrevendo no Arquivo...");
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path2))) {
+			for (Produto p : produtos) {
+				bw.write(p.getName() + ", " + p.sumTotal());
+				bw.newLine();
+				System.out.println("Escrita realizada com sucesso!!!");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		////////////////////////////////////////////////////////////////////////
+		//Leitura da escrita
+		System.out.println("//////////////////////////////////////////////////");
+		System.out.println();
+		System.out.println("Leitura do Arquivo:");
+		try (BufferedReader br = new BufferedReader(new FileReader(path2))) {
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 	}
 
