8b2e5fc02d83b6b319054faa7570a88266af3c4a
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-24 00:42:26 -0300

    (JAVA)Aulas 1 e 2 de JDBC + Organização de Pastas

---
+++ JAVA/Udemy/Aula_JDBC/Aula2/src/Application/Program.java
@@ -0,0 +1,33 @@
+package Application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("Select * from department");
+			
+			while(rs.next() != false) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
