661cc00ca0c0a508e1fa20310c1309377d7f0093
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-25 22:08:39 -0300

    (Java) Aula 5 Atualização

--- JAVA/FACENS/Aula5/src/ArvoreBinaria/App.java
+++ JAVA/FACENS/Aula5/src/ArvoreBinaria/App.java
@@ -1,24 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
+
         ArvoreBinaria minhaArvoreBinaria = new ArvoreBinaria();
 
+        minhaArvoreBinaria.adicionar(50);
+        minhaArvoreBinaria.adicionar(23);
+        minhaArvoreBinaria.adicionar(45);
+        minhaArvoreBinaria.adicionar(67);
+        minhaArvoreBinaria.adicionar(89);
+        minhaArvoreBinaria.adicionar(86);
+        minhaArvoreBinaria.adicionar(56);
+        minhaArvoreBinaria.adicionar(45);
+        minhaArvoreBinaria.adicionar(344);
 
-minhaArvoreBinaria.adicionar(50);
-minhaArvoreBinaria.adicionar(23);
-minhaArvoreBinaria.adicionar(45);
-minhaArvoreBinaria.adicionar(67);
-minhaArvoreBinaria.adicionar(89);
-minhaArvoreBinaria.adicionar(86);
-minhaArvoreBinaria.adicionar(56);
-minhaArvoreBinaria.adicionar(45);
-minhaArvoreBinaria.adicionar(344);
+        // int profundidade = minhaArvoreBinaria.buscarProfundidade(344);
+        //// System.out.println(profundidade);
 
-    //int profundidade = minhaArvoreBinaria.buscarProfundidade(344);
-////System.out.println(profundidade);
-
-
-       minhaArvoreBinaria.printArvore();
+        minhaArvoreBinaria.printArvore();
 
     }
 }
--- JAVA/FACENS/Aula5/src/ArvoreBinaria/ArvoreBinaria.java
+++ JAVA/FACENS/Aula5/src/ArvoreBinaria/ArvoreBinaria.java
@@ -2,7 +2,7 @@ public class ArvoreBinaria {
 
     private NoArvoreBinaria raiz;
 
-    public ArvoreBinaria(){
+    public ArvoreBinaria() {
         this.raiz = null;
     }
 
@@ -37,44 +37,92 @@ public class ArvoreBinaria {
     }
 
     public Integer buscarProfundidade(int info) {
-        return buscarProfundidadeRecursivo(raiz, info, 0); 
+        return buscarProfundidadeRecursivo(raiz, info, 0);
     }
 
-    private Integer buscarProfundidadeRecursivo(NoArvoreBinaria raiz,int info, int profundidade){
-        if(raiz == null){
+    private Integer buscarProfundidadeRecursivo(NoArvoreBinaria raiz, int info, int profundidade) {
+        if (raiz == null) {
             return -1;
         }
-        if(Integer.parseInt(raiz.getInfo().toString()) == info){
+        if (Integer.parseInt(raiz.getInfo().toString()) == info) {
             return profundidade;
         }
-        if(info < Integer.parseInt(raiz.getInfo().toString())){
+        if (info < Integer.parseInt(raiz.getInfo().toString())) {
             return buscarProfundidadeRecursivo(raiz.getNoEsquerda(), info, profundidade + 1);
-        }else{
+        } else {
             return buscarProfundidadeRecursivo(raiz.getNoDireita(), info, profundidade + 1);
         }
     }
 
-    public void printArvore(){
-        if(raiz != null){
-         printArvoreRecursivo(raiz, 0);
+    public void printArvore() {
+        if (raiz != null) {
+            printArvoreRecursivo(raiz, 0);
 
-        }else{
+        } else {
             System.out.println("A árvore está vazia!");
         }
 
     }
 
-    private void printArvoreRecursivo(NoArvoreBinaria no, int profundidade){
-        
-        if(no != null){
+    private void printArvoreRecursivo(NoArvoreBinaria no, int profundidade) {
+
+        if (no != null) {
             System.out.println("Profundidade: " + profundidade + ", info: " + no.getInfo());
-            
+
             printArvoreRecursivo(no.getNoEsquerda(), profundidade + 1);
 
             printArvoreRecursivo(no.getNoDireita(), profundidade + 1);
         }
-        
 
     }
 
+    public void preOrdem(NoArvoreBinaria no) {
+        if (no != null) {
+            System.err.println(no.getInfo());
+
+            preOrdem(no.getNoEsquerda());
+
+            preOrdem(no.getNoDireita());
+
+        }
+    }
+
+    public void emOrdem(NoArvoreBinaria no) {
+        if (no != null) {
+
+            preOrdem(no.getNoEsquerda());
+
+            System.err.println(no.getInfo());
+
+            preOrdem(no.getNoDireita());
+
+        }
+    }
+
+    public void posOrdem(NoArvoreBinaria no) {
+        if (no != null) {
+
+            preOrdem(no.getNoEsquerda());
+
+            preOrdem(no.getNoDireita());
+
+            System.err.println(no.getInfo());
+
+        }
+    }
+
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        arvore.adicionar(5);
+        arvore.adicionar(3);
+        arvore.adicionar(8);
+        arvore.adicionar(1);
+        arvore.adicionar(2);
+        arvore.adicionar(4);
+        arvore.adicionar(7);
+        arvore.adicionar(6);
+        arvore.adicionar(9);
+        // Imprime a árvore em ordem
+        arvore.emOrdem(arvore.raiz);
+    }
 }
